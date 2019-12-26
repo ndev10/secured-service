34b7a19a3d6ba37eb3eaa27d5496373feb0f01b3
ndev10 <ndev10@gmail.com>
2019-12-26 12:02:17 +0530

    Remove unused import.

--- src/main/java/com/secured/config/Oauth2ResourceServerConfig.java
+++ src/main/java/com/secured/config/Oauth2ResourceServerConfig.java
@@ -5,7 +5,6 @@ import com.secured.jwt.CustomTokenServices;
 import com.secured.jwt.JWTConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
